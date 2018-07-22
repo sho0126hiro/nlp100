73fc94e68cb275193a8db2a7370bbd13276d0616
sho0126hiro <sho0126hiro@yahoo.co.jp>
2018-07-22 17:33:37 +0900

    delete

--- Chapter1/test.py
+++
@@ -1,9 +0,0 @@
-str1 = "aa"
-str2 = 3
-str3 = [2]
-
-if type(str1) != str : print("true1")
-if type(str2) == int : print("true2")
-if type(str3) == list : print("true3")
-str2=str(str2)
-if type(str2) == str : print("true4")
