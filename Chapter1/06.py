f18f1d6102d6364893b06bf0509de0d5146967d9
sho0126hiro <sho0126hiro@yahoo.co.jp>
2018-07-22 15:07:30 +0900

    finish

--- Chapter1/06.py
+++ Chapter1/06.py
@@ -7,26 +7,51 @@ XとYの和集合，積集合，差集合を求めよ．
 """
 
 def ngram(target,n,type):
+    #(str,int,str)
     result=[]
+    list=target.split(' ')
     if type is "word":
         #単語:N-gram
-        for i in range(len(target)-n+1):
-            result.append(target[i:i+n])
+        for i in range(len(list)-n+1):
+            result.append(list[i:i+n])
     if type is "char":
         #文字:N-gram
-        list=target.split(' ')
         target=''.join(list)
         for i in range(len(target)-n+1):
             result.append(target[i:i+n])
     return result
-
+    
 def main():
     str1="paraparaparadise"
     str2="paragraph"
-    charbigram=[]
-    X=ngram(str1,2,"char")
-    Y=ngram(str2,2,"char")
-    print(char_ngram)
+
+    X = ngram(str1,2,"char")
+    Y = ngram(str2,2,"char")
+
+    X_set = set(X)
+    Y_set = set(Y)
+
+    print("X = {}".format(X_set))
+    print("Y = {}".format(Y_set))
+
+    XorY  = X_set | Y_set
+    XandY = X_set & Y_set
+    XdifY = X_set - Y_set
+    YdifX = Y_set - X_set
+
+    print("X | Y  = {}".format(XorY))
+    print("X & Y  = {}".format(XandY))
+    print("X - Y  = {}".format(XdifY))
+    print("Y - X  = {}".format(YdifX))
+
+    se=set(["se"])
+    print(se)
+    print(se and Y)
+    #<= subset : 部分集合　左辺の要素全てが右辺に含まれているか判定
+    if se <= X_set : print("Xにseは含まれています")
+    else           : print("Xにseは含まれていません")
+    if se <= Y_set : print("Yにseは含まれています")
+    else           : print("Yにseは含まれていません")
 
 if __name__  == '__main__':
     main()
