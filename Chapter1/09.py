9501c6f572dcb3fe4f9fe744f0bbdf907adf95fd
sho0126hiro <sho0126hiro@yahoo.co.jp>
2018-07-22 18:33:35 +0900

    finish

--- Chapter1/09.py
+++ Chapter1/09.py
@@ -8,31 +8,30 @@
 
 import random
 
-def replace(char):
-    # cher length : 2
-    char=list(char)
-    tmp=char[0]
-    char[0]=char[1]
-    char[1]=tmp
-    char="".join(char)
-    return char
-
 def main():
     msg="I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+    print(msg)
     l=msg.split(' ')
     result=[]
-    for i in range(len(l)) :
-        #各単語の先頭と末尾の文字以外を消す
-        if len(l[i]) > 2 :
-            result.append(l[i][0]+l[i][len(l[i])-1])
-        else : 
-            result.append(l[i]) 
-    print(result)
     #並び替え
-    for i in range(len(l)) : 
-        if len(l[i]) > 4 : 
-            if random.randrange(2) : result[i]=replace(result[i])
-    print(result)
+    #print(l)
+    for i in range(len(l)) :
+        if len(l[i]) > 4 :
+            l2=list(l[i]) #各単語をリスト型に変換
+            tmp=l2[1:len(l[i])-1]
+            random.shuffle(tmp)
+            l2[1:len(l[i])-1]=tmp[0:len(l[i])-2]
+            #print(l2)  #先頭と末尾以外でランダムに入れ替えたリスト型
+            l2="".join(l2)
+            result.append(l2)
+        else : 
+            result.append(l[i])
+    #print(result)
+    msg2=" ".join(result)
+    print(msg2)
+
+
+
 
 if __name__  == '__main__':
     main()
