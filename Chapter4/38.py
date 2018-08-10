40ced44add81d8ad01a18d051c6861cff526daba
sho0126hiro <sho0126hiro@gmail.com>
2018-08-10 13:26:16 +0900

    finish

--- Chapter4/38.py
+++ Chapter4/38.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-37. 頻度上位10語
-出現頻度が高い10語とその出現頻度をグラフ（例えば棒グラフなど）で表示せよ．
+38. ヒストグラム
+単語の出現頻度のヒストグラム（横軸に出現頻度，縦軸に出現頻度をとる単語の種類数を棒グラフで表したもの）を描け．
 
 棒グラフの出力例 : https://pythondatascience.plavox.info/matplotlib/%E6%A3%92%E3%82%B0%E3%83%A9%E3%83%95
 Hou to use 'numpy' : https://qiita.com/jyori112/items/a15658d1dd17c421e1e2
@@ -20,27 +20,16 @@ def main():
         data=f.readlines()
     word=[]
     frequency=[]
-    for i in range(10):
-        data[i]=data[i].replace("\n","")
-        word.append(data[i].split("\t")[0])
-        frequency.append(int(data[i].split("\t")[1]))
-    # print(word)
-    # ['の', 'て', 'は', 'に', 'を', 'と', 'が', 'た', 'で', 'も']
-    # print(frequency)
-    # ['9108', '6703', '6385', '6190', '6068', '5483', '5260', '3916', '3780', '2434']
+    for line in data:
+        line=line.replace("\n","")
+        word.append(line.split("\t")[0])
+        frequency.append(int(line.split("\t")[1]))
+    frequency_int=[]
+    #str >> int
     left=word
     right=frequency
     plt.bar(left,right)
-    plt.title("頻度上位10位")
-    plt.xlabel("出現頻度が高い10語")
-    plt.ylabel("出現頻度")
     plt.savefig( '37.png' )
-    # print(matplotlib.matplotlib_fname())
-    # /home/sho0126hiro/.local/lib/python3.5/site-packages/matplotlib/mpl-data/matplotlibrc
-    # print(matplotlib.get_configdir())
-    # /home/sho0126hiro/.config/matplotlib
-    # print(matplotlib.rcParams['font.family'])
-
 
 if __name__  == '__main__':
     main()
