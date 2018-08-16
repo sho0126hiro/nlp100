61785f40642f5e6dd52a201207ee3639a9ce4320
sho0126hiro <sho0126hiro@gmail.com>
2018-08-16 18:05:26 +0900

    finish

--- Chapter5/40.py
+++ Chapter5/40.py
@@ -22,6 +22,10 @@ class Morph:
         self.base = base
         self.pos = pos
         self.pos1 = pos1
+        # .surface = 'EOS'の時
+        # .base  count=num   num   文目の終了（存在しない）
+        # .pos   count=num   num+1 文目が次から始まる
+        # .pos1  'EOS'
     def printMorph(self):
         print("surface = {0} / base = {1} / pos = {2} / pos1 = {3}".format(self.surface,self.base,self.pos,self.pos1))
 
@@ -32,30 +36,61 @@ def dependency_parsing():
     c = CaboCha.Parser()
     tree=c.parse(text)
     result=tree.toString(CaboCha.FORMAT_LATTICE)
-    data=result.split('\n')
     with open("40_neko.txt.cabocha",mode='w') as c:
-        for i in range(len(data)):
-            c.write("".join(data[i]))
-            c.write("\n")
+        c.write(result)
 
 def init_list():
     with open("40_neko.txt.cabocha",mode='r') as f:
         result=f.readlines()
-    morph=[]
+    morphs=[]
+    # .surface = 'EOS'の時
+    # .base  count=0 0 文目の終了（存在しない）
+    # .pos   count=0+1 文目が次から始まる
+    # .pos1  'EOS'
+    count=0
+    morphs.append(Morph('EOS',str(count),str(count+1),'EOS'))
     for line in result:
         if line == "EOS\n" or line[0]=='*':
-            continue
-        if len(line.split('\t')) > 1 :
-            surface=line.split('\t')[0]
-            tmp=line.split('\t')[1]
-            base=tmp.split(',')[6]
-            pos=tmp.split(',')[0]
-            pos1=tmp.split(',')[1]
-            morph.append(Morph(surface,base,pos,pos1))
-    morph[2].printMorph()
+            continue # 処理しない行
+        surface=line.split('\t')[0]
+        tmp=line.split('\t')[1]
+        base=tmp.split(',')[6]
+        pos=tmp.split(',')[0]
+        pos1=tmp.split(',')[1]
+        morphs.append(Morph(surface,base,pos,pos1))
+        if surface == '。' :
+            count+=1
+            morphs.append(Morph('EOS',str(count),str(count+1),'EOS'))
+            # .surface = 'EOS'の時
+            # .base  count=num   num   文目の終了（存在しない）
+            # .pos   count=num   num+1 文目が次から始まる
+            # .pos1  'EOS'
+    return morphs
 def main():
-    #dependency_parsing()
-    init_list()
+    # dependency_parsing()
+    morphs=init_list()
+    #三文目の形態素列を示す
+    for i in range(len(morphs)):
+        if morphs[i].surface == 'EOS' and morphs[i].pos == '3' :
+            i+=1
+            while not(morphs[i].surface == 'EOS') :
+                morphs[i].printMorph()
+                i+=1
 
 if __name__  == '__main__':
     main()
+
+"""
+surface = 　 / base = 　 / pos = 記号 / pos1 = 空白
+surface = どこ / base = どこ / pos = 名詞 / pos1 = 代名詞
+surface = で / base = で / pos = 助詞 / pos1 = 格助詞
+surface = 生れ / base = 生れる / pos = 動詞 / pos1 = 自立
+surface = た / base = た / pos = 助動詞 / pos1 = *
+surface = かとん / base = 火遁 / pos = 名詞 / pos1 = 一般
+surface = と / base = と / pos = 助詞 / pos1 = 格助詞
+surface = 見当 / base = 見当 / pos = 名詞 / pos1 = サ変接続
+surface = が / base = が / pos = 助詞 / pos1 = 格助詞
+surface = つか / base = つく / pos = 動詞 / pos1 = 自立
+surface = ぬ / base = ぬ / pos = 助動詞 / pos1 = *
+surface = 。 / base = 。 / pos = 記号 / pos1 = 句点
+"""
